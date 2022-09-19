aa9a79ca6e4c4a0b417d810f05e98f3027733059
LethaboMokg <lethabo1.mokg@gmal.com>
2022-09-19 04:47:20 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototype()
+ * int prototypes()
+ */
+
+void reset to 98(int *n);
+void swap int( *a, int *b);
+int _strlen(char *s);
+void puts(char *str);
+void rev_string(char *s);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
